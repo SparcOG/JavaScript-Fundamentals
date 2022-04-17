a742eeff56597825ea101619e9871f539ffad04a
Igor Stashok <glurmxm@gmail.com>
2022-04-17 13:26:53 +0200

    урок лет конст и вар

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,18 @@
-let country = "Ukranine";
-let continent = "Europ";
+const country = "Ukranine";
+const continent = "Europ";
 let population = "44 mln";
+language = "Украинский";
 
-console.log(country);
-console.log(continent);
+const isIsland = "Ukraine";
+
+console.log(isIsland);
 console.log(population);
+console.log(country);
+console.log(language);
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// isIsland = true;
+
+// let language;
