848affe6dc4704204d74c1dbd28b34d3b58b211e
Igor Stashok <glurmxm@gmail.com>
2022-04-17 19:08:26 +0200

    урок про операторы присваивания и сравнения

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,10 +1,32 @@
 const country = "Ukranine";
 const continent = "Europ";
-let population = "44 mln";
+let population = 44;
+let finland = 6;
+let averagePopulation = 33;
 language = "Украинский";
+const description =
+  country +
+  " " +
+  "is in" +
+  " " +
+  continent +
+  " " +
+  "and its" +
+  " " +
+  population +
+  " " +
+  "people speak" +
+  " " +
+  language;
+population = population / 2;
+
+population++;
 
 const isIsland = "Ukraine";
 
+console.log(description);
+console.log(population < averagePopulation);
+console.log(population > finland);
 console.log(isIsland);
 console.log(population);
 console.log(country);
