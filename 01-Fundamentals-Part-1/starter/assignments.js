f2e38e4e7e889cb0869278c689d640b5dd694bed
Igor Stashok <glurmxm@gmail.com>
2022-04-18 18:27:32 +0200

    Урок 1 решен

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,36 +1,37 @@
-const country = "Ukranine";
-const continent = "Europ";
-let population = 44;
-let finland = 6;
-let averagePopulation = 33;
-language = "Украинский";
-const description =
-  country +
-  " " +
-  "is in" +
-  " " +
-  continent +
-  " " +
-  "and its" +
-  " " +
-  population +
-  " " +
-  "people speak" +
-  " " +
-  language;
-population = population / 2;
+// = 78 / (169 * 169);
+// const country = "Ukranine";
+// const continent = "Europ";
+// let population = 44;
+// let finland = 6;
+// let averagePopulation = 33;
+// language = "Украинский";
+// const description =
+//   country +
+//   " " +
+//   "is in" +
+//   " " +
+//   continent +
+//   " " +
+//   "and its" +
+//   " " +
+//   population +
+//   " " +
+//   "people speak" +
+//   " " +
+//   language;
+// population = population / 2;
 
-population++;
+// population++;
 
-const isIsland = "Ukraine";
+// const isIsland = "Ukraine";
 
-console.log(description);
-console.log(population < averagePopulation);
-console.log(population > finland);
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language);
+// console.log(description);
+// console.log(population < averagePopulation);
+// console.log(population > finland);
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
 
 // console.log(country);
 // console.log(continent);
@@ -38,3 +39,14 @@ console.log(language);
 // isIsland = true;
 
 // let language;
+
+const marksWeights = 78;
+const marksTall = 169;
+const johnsWeights = 92;
+const johnTall = 195;
+
+const marksIbm = marksWeights / marksTall ** 2;
+const johnIbm = johnsWeights / johnTall ** 2;
+
+const markHigherBMI = marksIbm > johnIbm;
+console.log(marksIbm, johnIbm, markHigherBMI);
