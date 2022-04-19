e5b30943c56722446bd40e8092f286f6499e9d3f
Igor Stashok <glurmxm@gmail.com>
2022-04-19 16:07:59 +0200

    Coding Challenge #2

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,7 +1,7 @@
 // = 78 / (169 * 169);
-const country = "Ukranine";
+// const country = "Ukranine";
 // const continent = "Europ";
-
+// let population = 44;
 // let finland = 6;
 // let averagePopulation = 33;
 // language = "Украинский";
@@ -20,11 +20,8 @@ const country = "Ukranine";
 //   " " +
 //   language;
 // population = population / 2;
-
 // population++;
-
 // const isIsland = "Ukraine";
-
 // console.log(description);
 // console.log(population < averagePopulation);
 // console.log(population > finland);
@@ -32,28 +29,32 @@ const country = "Ukranine";
 // console.log(population);
 // console.log(country);
 // console.log(language);
-
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 // isIsland = true;
-
 // let language;
 
-// const marksWeights = 78;
-// const marksTall = 169;
-// const johnsWeights = 92;
-// const johnTall = 195;
+// if (population >= 120) {
+//   console.log(`${country} population is above average`);
+// } else {
+//   console.log(`${country} population is ${120 - population} below average`);
+// }
 
-// const marksIbm = marksWeights / marksTall ** 2;
-// const johnIbm = johnsWeights / johnTall ** 2;
+const marksWeights = 78;
+const marksTall = 169;
+const johnsWeights = 92;
+const johnTall = 195;
 
-// const markHigherBMI = marksIbm > johnIbm;
-// console.log(marksIbm, johnIbm, markHigherBMI);
+const marksIbm = marksWeights / marksTall ** 2;
+const johnIbm = johnsWeights / johnTall ** 2;
 
-let population = 44;
-if (population >= 120) {
-  console.log(`${country} population is above average`);
+const markHigherBMI = marksIbm > johnIbm;
+
+if (marksIbm > johnIbm) {
+  console.log(`Mark's BMI (${marksIbm}) is higher than John's (${johnIbm})`);
 } else {
-  console.log(`${country} population is ${120 - population} below average`);
+  console.log("John's BMI is higher than Mark's!");
 }
+
+console.log(marksIbm, johnIbm, markHigherBMI);
