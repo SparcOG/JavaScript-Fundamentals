fdc8e17ac11de3e1cbf52e9ada1af2bc735b6b10
Igor Stashok <glurmxm@gmail.com>
2022-04-22 20:21:34 +0200

    Equality Operators: == vs. ===

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -59,8 +59,20 @@
 
 // console.log(marksIbm, johnIbm, markHigherBMI);
 
-console.log("9" - "5"); //4
-console.log("19" - "13" + "17"); //617
-console.log("19" - "13" + 17); //23
-console.log("123" < 57); //fals
-console.log(5 + 6 + "4" + 9 - 4 - 2); //1143
+// console.log("9" - "5"); //4
+// console.log("19" - "13" + "17"); //617
+// console.log("19" - "13" + 17); //23
+// console.log("123" < 57); //fals
+// console.log(5 + 6 + "4" + 9 - 4 - 2); //1143
+
+const numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?")
+);
+
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
