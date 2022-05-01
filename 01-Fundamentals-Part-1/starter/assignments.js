a422edde980cb16fc27d1aab05634e2ed063bcad
Igor Stashok <glurmxm@gmail.com>
2022-05-01 20:00:44 +0200

    троичный оператор

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -144,25 +144,31 @@
 //   console.log("Not't a valid day!");
 // }
 
-const language = "sdfvsdfv";
+// const language = "sdfvsdfv";
 
-switch (language) {
-  case "chinese":
-  case "mandarin":
-    console.log("MOST number of native speakers!");
-    break;
-  case "spanish":
-    console.log("2nd place in number of native speakers");
-    break;
-  case "english":
-    console.log("3rd place");
-    break;
-  case "hindi":
-    console.log("Number 4");
-    break;
-  case "arabic":
-    console.log("5th most spoken language");
-    break;
-  default:
-    console.log("Great language too 😁");
-}
+// switch (language) {
+//   case "chinese":
+//   case "mandarin":
+//     console.log("MOST number of native speakers!");
+//     break;
+//   case "spanish":
+//     console.log("2nd place in number of native speakers");
+//     break;
+//   case "english":
+//     console.log("3rd place");
+//     break;
+//   case "hindi":
+//     console.log("Number 4");
+//     break;
+//   case "arabic":
+//     console.log("5th most spoken language");
+//     break;
+//   default:
+//     console.log("Great language too 😁");
+// }
+
+const country = "Ukranine";
+const population = 44;
+population >= 33
+  ? console.log(`${country} population is above average`)
+  : console.log(`${country} population is below average`);
