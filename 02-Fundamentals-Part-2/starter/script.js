2b8423f5e924adb122b98595c166570f8fa8442a
Igor Stashok <glurmxm@gmail.com>
2022-05-04 19:36:45 +0200

    Functions Jonas

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,16 +4,25 @@
 // const population = 44;
 // const capitalCity = "Kiev";
 
-function descripteCountry(country, population, capitalCity) {
-  const descripte = `${country} has ${population} million people and its capital city is ${capitalCity}`;
-  return descripte;
+// function descripteCountry(country, population, capitalCity) {
+//   const descripte = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+//   return descripte;
+// }
+
+// const OneDescripte = descripteCountry("Ukraine", 44, "Kiev");
+// console.log(OneDescripte);
+
+// const TwoDescripte = descripteCountry("Poland", 37, "Warsaw");
+// console.log(TwoDescripte);
+
+// const ThreeDescripte = descripteCountry("Canada", 38, "Ottawa");
+// console.log(ThreeDescripte);
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and
+  its capital city is ${capitalCity}`;
 }
-
-const OneDescripte = descripteCountry("Ukraine", 44, "Kiev");
-console.log(OneDescripte);
-
-const TwoDescripte = descripteCountry("Poland", 37, "Warsaw");
-console.log(TwoDescripte);
-
-const ThreeDescripte = descripteCountry("Canada", 38, "Ottawa");
-console.log(ThreeDescripte);
+const descPortugal = describeCountry("Portugal", 10, "Lisbon");
+const descGermany = describeCountry("Germany", 83, "Berlin");
+const descFinland = describeCountry("Finland", 6, "Helsinki");
+console.log(descPortugal, descGermany, descFinland);
