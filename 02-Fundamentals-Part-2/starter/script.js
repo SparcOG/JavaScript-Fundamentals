baf11bc9b8f2845c0024f0af86299e078200e6a7
Igor Stashok <glurmxm@gmail.com>
2022-05-14 21:29:11 +0200

    Осталось написать бонусное задание

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -201,5 +201,9 @@ const calcTip = function (bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const tips = [
+  `Tipping is ${calcTip(bills[0])} dollars`,
+  `Tipping is ${calcTip(bills[1])} dollars`,
+  `Tipping is ${calcTip(bills[2])} dollars`,
+];
 console.log(tips);
