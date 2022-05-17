38a0fb2817c8222b431a31c583440339bc6d49c5
Igor Stashok <glurmxm@gmail.com>
2022-05-17 14:09:38 +0200

    Сompleted the LECTURE: Dot vs. Bracket Notatio Jonasn

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -262,9 +262,22 @@ const myCountry = {
   ],
 };
 
-myCountry.population = myCountry.population + 2;
-myCountry["population"] = myCountry["population"] - 2;
+// myCountry.population = myCountry.population + 2;
+// myCountry["population"] = myCountry["population"] - 2;
+
+// console.log(
+//   `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+// );
 
 console.log(
-  `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
+  `${myCountry.country} has ${myCountry.population} million
+  ${myCountry.language}-speaking people,
+  ${myCountry.neighbours.length} neighbouring countries and
+  a capital called ${myCountry.capital}.`
 );
+
+myCountry.population += 2;
+console.log(myCountry.population);
+
+myCountry["population"] -= 2;
+console.log(myCountry.population);
