0bcea9f50d5ba83486ef39f14261091c13b3be17
Igor Stashok <glurmxm@gmail.com>
2022-05-28 19:38:56 +0200

    Coding Challenge #4: Jonas  version + bonus

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -658,15 +658,33 @@
 //   totals.push(tips[i] + bills[i]);
 // }
 
-// console.log(tips, totals);
+// console.log(bills, tips, totals);
 
-const arr = [453, 543, 12, 57, 56];
-let sum = 0;
+//Jonas----------.....
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 
-for (let i = 0; i < arr.length; i = i + 1) {
-  sum = sum + arr[i];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bills) {
+  return bills >= 50 && bills <= 300 ? bills * 0.15 : bills * 0.2;
+};
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
 }
 
-const calcAverage = (sum) => sum / arr.length;
+console.log(bills, totals, tips);
 
-console.log(calcAverage(sum));
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i = i + 1) {
+    sum = sum + arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
