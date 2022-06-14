308771b3252b4f0ac6a9dc44f20799f3f1894d1b
Igor Stashok <glurmxm@gmail.com>
2022-06-14 19:26:14 +0200

    Section 7 - JavaScript in the Browser: DOM and Events Fundamentals: Coding Challenge #1: fix bag whith score Jonas

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,26 +12,7 @@ console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-// Reset game
-document.querySelector('.again').addEventListener('click', function () {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
-});
-
-// document.querySelector('.again').addEventListener('click', function () {
-//   document.querySelector('.number').textContent = '?';
-//   document.querySelector('.number').style.width = '15rem';
-//   document.querySelector('.score').textContent = 20;
-//   document.querySelector('.message').textContent = 'Start guessing...';
-//   document.querySelector('body').style.backgroundColor = '#222';
-//   document.querySelector('.guess').value = '';
-// });
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -47,6 +28,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too hight
   } else if (guess > secretNumber) {
@@ -71,3 +57,24 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
+
+// Reset game
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
+
+// document.querySelector('.again').addEventListener('click', function () {
+//   document.querySelector('.number').textContent = '?';
+//   document.querySelector('.number').style.width = '15rem';
+//   document.querySelector('.score').textContent = 20;
+//   document.querySelector('.message').textContent = 'Start guessing...';
+//   document.querySelector('body').style.backgroundColor = '#222';
+//   document.querySelector('.guess').value = '';
+// });
