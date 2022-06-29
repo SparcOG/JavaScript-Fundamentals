e28e5f3a10d9ff3aa216802019faadf9eb7a546c
Igor Stashok <glurmxm@gmail.com>
2022-06-29 21:09:42 +0200

    Section 7 - JavaScript in the Browser: DOM and Events Fundamentals: Lecture 83: PROJECT #3: Rolling the Dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,10 +1,46 @@
 'use strict';
 
-//Selecting elements
+// Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+let currentScore = 0;
+
+// Roling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if tru, swich to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    // currentScore = currentScore + dice;
+    currentScore += dice; // it will be the same string up (short version)
+    current0El.textContent = currentScore;
+  } else if (dice === 1) {
+    current0El.textContent = 0;
+  } else if (dice == 1) {
+    currentScore += dice;
+    current1El.textContent = currentScore;
+  } else {
+    // Switch to next player
+    // I'm tri do it one
+  }
+});
