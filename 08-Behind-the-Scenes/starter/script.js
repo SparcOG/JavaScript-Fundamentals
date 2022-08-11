7fb2e8520686159dd3d8f30b360a20b2ed577e25
Igor Stashok <glurmxm@gmail.com>
2022-08-10 22:05:25 -0400

    Section 8 - How JavaScript Works Behind the Scenes: Lecture 93: Scoping in Practice - continuation part 1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -2,7 +2,21 @@
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
-  console.log(firstName);
+
+  function printAge() {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+    }
+    // console.log(str);
+    console.log(millenial);
+  }
+  printAge();
+
   return age;
 }
 
