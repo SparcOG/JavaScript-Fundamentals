f9317b7f2092bbcba6324a66934c73258d02ed30
Igor Stashok <glurmxm@gmail.com>
2022-08-17 19:29:40 -0400

    Section 8 - How JavaScript Works Behind the Scenes: Lecture 95: Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,35 +1,60 @@
-// 'use strict';
+// // 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `You are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `You are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      // Creating NEW variable with same name as outer scope's variable
-      const firstName = 'Steven';
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       // Creating NEW variable with same name as outer scope's variable
+//       const firstName = 'Steven';
 
-      // Reassigning outher scope's variable
-      output = 'NEW OUTPUT!';
+//       // Reassigning outher scope's variable
+//       output = 'NEW OUTPUT!';
 
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str);
-    console.log(millenial);
-    console.log(output);
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(str);
+//     console.log(millenial);
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = 'Jonas';
+// calcAge(1991);
+
+//Variables
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Jonas';
-calcAge(1991);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
