1dff0bd269c2d79393081d09ffa03f14cec6c5bc
Igor Stashok <glurmxm@gmail.com>
2022-08-17 18:31:31 -0400

    Section 8 - How JavaScript Works Behind the Scenes: Lecture 93: Scoping in Practice - continuation part 2

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,19 +1,30 @@
-'use strict';
+// 'use strict';
 
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    let output = `You are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
       var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reassigning outher scope's variable
+      output = 'NEW OUTPUT!';
+
       const str = `Oh, and you're a millenial, ${firstName}`;
       console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
     }
     // console.log(str);
     console.log(millenial);
+    console.log(output);
   }
   printAge();
 
