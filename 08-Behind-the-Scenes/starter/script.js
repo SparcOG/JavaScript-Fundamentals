b22a1d3e3bab859dc5e185f27637f081bb491c43
Igor Stashok <glurmxm@gmail.com>
2022-08-18 18:57:01 -0400

    Section 8 - How JavaScript Works Behind the Scenes: Lecture 95: Hoisting and TDZ in Practice part 1

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -35,26 +35,43 @@
 // calcAge(1991);
 
 //Variables
-console.log(me);
-console.log(job);
-console.log(year);
+// console.log(me);
+// console.log(job);
+// console.log(year);
 
-var me = 'Jonas';
-let job = 'teacher';
-const year = 1991;
+// var me = 'Jonas';
+// let job = 'teacher';
+// const year = 1991;
 
-// Functions
-console.log(addDecl(2, 3));
-// console.log(addExpr(2, 3));
-console.log(addArrow);
-// console.log(addArrow(2, 3));
+// // Functions
+// console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// console.log(addArrow);
+// // console.log(addArrow(2, 3));
 
-function addDecl(a, b) {
-  return a + b;
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShopingCard();
+
+var numProducts = 10;
+
+function deleteShopingCard() {
+  console.log('All products deleted!');
 }
 
-const addExpr = function (a, b) {
-  return a + b;
-};
+var x = 1;
+let y = 2;
+const z = 3;
 
-var addArrow = (a, b) => a + b;
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
