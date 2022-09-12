7ee70bfec8ce7de5fe3cf5999eb543d703843d34
Igor Stashok <glurmxm@gmail.com>
2022-09-12 19:03:17 -0400

    Section 9 - How JavaScript Works Behind the Scenes: 110. Coding Challenge #1 almost did

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -269,7 +269,7 @@ console.log(mainMenuCopy);
 // console.log(p, q, r);
 */
 
-
+//////////////////////////////////////////////////////////////
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -310,157 +310,45 @@ const game = {
     team2: 6.5,
   },
 };
-/*
+
+
+// 1)
 const players1 = game.players[0];
 const players2 = game.players[1];
 console.log(players1, players2);
 
+// 2)
 const gk = game.players[0][0];
 console.log(gk);
 
+// 3)
 const fieldPlayers = [, ...game.players[0]];
 console.log([, ...fieldPlayers]);
 
 const allPlayers = [...players1, ...players2];
 console.log(allPlayers);
 
-// const arr = [7, 8, 9];
-// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-// console.log(badNewArr);
-
+// 4)
 const players1Final = [game.players[0], 'Thiago', 'Coutinho', 
 'Perisic'];
 
+
+// 5)
 const [team1, drow, team2] = [game.odds.team1, game.odds.x, 
   game.odds.team2];
-*/
-// const printGoals = function (...players) {
-//   let sum = 0;
-//   for (let i = 0; i = players.length; i++) {
-//     sum = sum += players[i]
-//     console.log(sum)
-//   }
-// }
-// printGoals(...game.players)
 
-// // 2) Functions
-// const add = function (...numb3rs) {
-//   let sum = 0;
-//   for (let i = 0; i < numb3rs.length; i++) sum = sum += numb3rs[i];
-//   console.log(sum);
-// };
 
+// 6)
 const printGoals = function (...players) {
   let sum = 0;
   for (let i = 0; i < players.length; i++) {
-    sum =+ players[i]
 console.log(...players[i], players[i].length)
   }
 }
+printGoals(['Davies', 'Muller', 'Lewandowski', 'Kimmich'])
 printGoals(...game.players)
 
-// 2) Functions
-// const add = function (...numb3rs) {
-//   let sum = 0;
-//   for (let i = 0; i < numb3rs.length; i++) {
-//     sum = sum += numb3rs[i];
-//   } 
-//   console.log(numb3rs.length);
-// };
-
-// add(2, 3);
-// add(5, 3, 7, 2);
-// add(8, 2, 5, 3, 2, 1, 4);
-
-// const x = [23, 5, 7];
-// add(...x);
-
-// const arr = [2, 3, 4];
-// const a = arr[0];
-// const b = arr[1];
-// const c = arr[2];
-
-// const [x, y, z] = arr;
-// console.log(x, y, z);
+// 7)
+console.log(game.odds.team2 && game.odds.team1)
 
 
-
-
-
-/*
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-    );
-  },
-
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `Here is your declicionus pasta with ${ing1}, ${ing2} and ${ing3}`
-    );
-  },
-};
-
-const arr = [7, 8, 9];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr);
-
-const newArr = [1, 2, ...arr];
-console.log(...newArr);
-
-console.log(...newArr);
-console.log(1, 2, 7, 8, 9);
-
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
-
-// Copy array
-const mainMenuCopy = [...restaurant.mainMenu];
-console.log(mainMenuCopy);
-
-// Join 2 arrays
-const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
-console.log(menu);
-
-// Iterables: arrays, strings, maps, sets. NOT objects
-const str = 'Jonas';
-const letters = [...str];
-console.log(letters);
-console.log(...str);
-// console.log(`${...str}`);
-
-const ingredients = [
-  prompt("Let's make pasta! Ingredient 1?"),
-  prompt('Ingredient 2?'),
-  prompt('Ingredient 3'),
-];
-console.log(ingredients);
-
-restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
-restaurant.orderPasta(...ingredients);
-*/
