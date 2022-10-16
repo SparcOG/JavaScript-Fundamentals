54cd2357bcc10ca7836096071855d51b3405c436
Igor Stashok <glurmxm@gmail.com>
2022-10-16 19:57:07 -0400

    Section 7 - JavaScript in the Browser: DOM and Events Fundamentals: 120. Coding Challenge #3 121. Working With Strings - Part 1

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -49,6 +49,33 @@ const restaurant = {
   },
 };
 
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]);
+console.log(plane[1]);
+console.log(plane[2]);
+console.log('B737'[0]);
+
+console.log(airline.length);
+console.log('B737'.length);
+
+console.log(airline.indexOf('r'));
+console.log(airline.lastIndexOf('r'));
+console.log(airline.indexOf('portugal'));
+
+console.log(airline.slice(4));
+console.log(airline.slice(4, 7));
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2));
+console.log(airline.slice(1, -1));
+
+//////////////////////////////////////////////
+/////// Coding challenge #3
+/*
 const gameEvents = new Map([
   [17, '⚽ GOAL'],
   [36, '� Substitution'],
@@ -128,7 +155,7 @@ for (const [min, event] of gameEvents) {
 // // console.log(question.entries());
 // console.log([...question.keys()]);
 // console.log([...question.values()]);
-
+*/
 /*
 ///////////////////////////////////////////////////////////////
 ////// Maps: Interation
