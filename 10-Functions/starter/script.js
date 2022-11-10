7b41661dd465a6014533109dd0217cfc348f6fa7
Igor Stashok <glurmxm@gmail.com>
2022-11-09 21:26:53 -0500

    Section 10: A Closer Look at Functions: 129. How Passing Arguments Works: Value vs. Reference 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -43,4 +43,17 @@ const checkIn = function (flightNum, passenger) {
   }
 };
 
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
 checkIn(flight, jonas);
