d584b3b8dd7dbf4b826b7037a56023877e7f4d87
Igor Stashok <glurmxm@gmail.com>
2022-11-15 21:38:49 -0500

    Section 10: A Closer Look at Functions: const greet = function (greeting) {

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -26,34 +26,81 @@
 
 // createBooking('LH123', undefined, 1000);
 
-const flight = 'LH234';
-const jonas = {
-  name: 'Jonas Schmedtmann',
-  passport: 234234234523,
-};
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 234234234523,
+// };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 234234234523) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport!');
-  }
-};
+//   if (passenger.passport === 234234234523) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
 
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+
+// newPassport(jonas);
 // checkIn(flight, jonas);
-// console.log(flight);
-// console.log(jonas);
 
-// Is the same as doing...
-// const flightNum = flight;
-// const passenger = jonas;
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 100000000000);
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// // Higher-order function
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('JavaScript is the best!', upperFirstWord);
+
+// transformer('JavaScript is the best', oneWord);
+
+// const high5 = function () {
+//   console.log('👍');
+// };
+
+// document.body.addEventListener('click', high5);
+
+// ['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
 };
 
-newPassport(jonas);
-checkIn(flight, jonas);
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello');
+greet('Hello')('Jonas');
+
+// Challenge
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('Jonas');
