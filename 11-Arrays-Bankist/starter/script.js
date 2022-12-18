8a4fd1a8d8f354284f5cbc14016fad96f1679f31
Igor Stashok <glurmxm@gmail.com>
2022-12-18 12:57:24 -0500

    Section 11: Working With Arrays - 145. forEach With Maps and Sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
 /////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////
@@ -129,6 +123,7 @@ console.log('jonas'.at(-1));
 */
 /////////////////////////////////////////////////////////////////////////
 ////////////////////////// 144. Looping Arrays: forEach
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 for (const [i, movement] of movements.entries()) {
@@ -150,3 +145,17 @@ movements.forEach(function (mov, i, arr) {
 // 0: function(200)
 // 1: function(450)
 // 2: function(400)
+*/
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
