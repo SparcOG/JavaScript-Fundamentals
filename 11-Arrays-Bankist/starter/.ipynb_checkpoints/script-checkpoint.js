92fa071b30c3323cf2737056060c75a820c0800a
SparcOG <96662655+SparcOG@users.noreply.github.com>
2022-12-28 01:37:51 +0000

    Section 11: Working With Arrays - 150. The map Method

--- 11-Arrays-Bankist/starter/.ipynb_checkpoints/script-checkpoint.js
+++ 11-Arrays-Bankist/starter/.ipynb_checkpoints/script-checkpoint.js
@@ -291,19 +291,3 @@ const movementsUSD = movements.map(function (mov) {
   return mov * eurToUsd;
 });
 console.log(movements);
-console.log(movementsUSD);
-const movementUSD = movements.map(mov => mov * eurToUsd);
-console.log(movements);
-console.log(movementUSD);
-
-const movementUSDfor = [];
-for (const mov of movements) movementUSDfor.push(mov * eurToUsd);
-console.log(movementUSDfor);
-
-const movementsDescriptions = movements.map(
-  (mov, i) =>
-    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
-      mov
-    )}`
-);
-console.log(movementsDescriptions);
