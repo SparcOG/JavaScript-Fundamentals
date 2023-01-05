99e3d3b8cbed655f2954423ac5bb2125ec916d45
SparcOG <96662655+SparcOG@users.noreply.github.com>
2023-01-05 00:18:56 +0000

    Section 11: Working With Arrays - 155. The Magic of Chaining Methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -89,6 +89,21 @@ const calclDisplaySummary = function (movements) {
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes} 💶`;
+
+  const out = movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)} 💶`;
+
+  const interest = movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest} 💶`;
 };
 calclDisplaySummary(account1.movements);
 
