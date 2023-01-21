ec27a819eaf16626af88947037e19a456c6a9dc8
SparcOG <96662655+SparcOG@users.noreply.github.com>
2023-01-21 20:43:50 +0000

    Section 11: Working With Arrays - 164. More Ways of Creating and Filling Arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -713,6 +713,7 @@ console.log(overBalance2);
 ////////////////////////////////////////////////////////////////////////////////
 // Section 11: Working With Arrays - 163. Sorting Arrays
 ////////////////////////////////////////////////////////////////////////////////
+/*
 const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
 console.log(owners.sort());
 
@@ -737,3 +738,38 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+*/
+////////////////////////////////////////////////////////////////////////////////
+// Section 11: Working With Arrays - 164. More Ways of Creating and Filling Arrays
+////////////////////////////////////////////////////////////////////////////////
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+// x.fill(1);
+x.fill(1, 2, 5);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('-', '+'))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+  console.log(movementsUI2);
+});
