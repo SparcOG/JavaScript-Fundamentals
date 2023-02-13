412429648e8bad4fc52125cb98b9b602a0fdad1a
glurmxm <glurmxm@gmail.com>
2023-02-13 18:53:28 -0500

    Section 12: Numbers Dates Intl and Timers - 177. Operations With Dates (Work on app)

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -21,9 +21,9 @@ const account1 = {
     '2020-01-28T09:15:04.904Z',
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2023-02-06T17:01:17.194Z',
+    '2023-02-08T23:36:17.929Z',
+    '2023-02-13T10:51:36.790Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -83,16 +83,20 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 const formatMovementDate = function (date) {
   const calcDaysPassed = (date1, date2) =>
-    Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
   const daysPassed = calcDaysPassed(new Date(), date);
   console.log(daysPassed);
 
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getDate() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year}`;
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getDate() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 };
 
 const displayMovements = function (acc, sort = false) {
@@ -238,11 +242,10 @@ btnTransfer.addEventListener('click', function (e) {
 
     // Add transfer
     currentAccount.movementsDates.push(new Date().toISOString());
-    receiverAcc.movementsDates
-      .push(amount)
+    receiverAcc.movementsDates.push(amount);
 
-      // Update UI
-      .updateUI(currentAccount);
+    // Update UI
+    updateUI(currentAccount);
   }
 });
 
@@ -465,8 +468,4 @@ console.log(Date.now());
 ///////////////////////////176. Adding Dates to "Bankist" App (Work on app)
 
 ///////////////////////////////////////////////////////////////////
-///////////////////////////177. Operations With Dates
-const future = new Date(2037, 10, 19, 15, 23);
-console.log(+future);
-
-// console.log(days1);
+///////////////////////////177. Operations With Dates (Work on app)
