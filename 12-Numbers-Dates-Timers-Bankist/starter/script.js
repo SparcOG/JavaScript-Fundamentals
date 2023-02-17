0f9fcfc7f5950f8841c2fb2ee4bdb7b8f5f565e5
glurmxm <glurmxm@gmail.com>
2023-02-16 19:09:38 -0500

    Section 12: Numbers Dates Intl and Timers - 179. Internationalizing Numbers (Intl) (Work on app) and more

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -508,3 +508,27 @@ console.log(Date.now());
 
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////179. Internationalizing Numbers (Intl) (Work on app)
+const num = 3884764.23;
+
+const options = {
+  style: 'currency',
+  unit: 'celsius',
+  currency: 'EUR',
+  // useGrouping: false,
+};
+console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num));
+console.log(
+  'Germany:      ',
+  new Intl.NumberFormat('de-DE', options).format(num)
+);
+console.log(
+  'Syria:      ',
+  new Intl.NumberFormat('ar-SY', options).format(num)
+);
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language, options).format(num)
+);
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////180. Timers: setTimeout and setInterval
