b964827ddeb25c6a72457f4abc260c7a7d48dce8
glurmxm <glurmxm@gmail.com>
2023-02-20 15:08:58 -0500

    Section 12: Numbers Dates Intl and Timers - 181. Implementing a Countdown Timer (Work on app)

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -589,3 +589,19 @@ setInterval(function () {
 */
 ///////////////////////////////////////////////////////////////////
 ///////////////////////////181. Implementing a Countdown Timer (Work on app)
+
+const ingredients = ['olives', 'spinach'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2} 🍕`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval
+setInterval(function () {
+  const now = new Date(5);
+  console.log(now.getHours(), now.getMinutes(), now.getSeconds());
+}, 1000);
