d6f0244bee31c03a0e0ddf08f9dc1bba1e21a380
glurmxm <glurmxm@gmail.com>
2023-02-21 18:52:42 -0500

    Section 13: Advanced DOM and Events - 187. Styles, Attributes and Classes

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -65,3 +65,20 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+//////////////////////////////////////////////////////////////////
+// 187. Styles, Attributes and Classes
+//Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.document.style.setProperty('--color-primary', 'orangered');
