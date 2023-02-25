c9a465a4d0482561c0d7ca8a344e1b5128432d90
glurmxm <glurmxm@gmail.com>
2023-02-24 19:02:29 -0500

    Section 13: Advanced DOM and Events - 188. Implementing Smooth Scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -68,6 +68,7 @@ document
 
 //////////////////////////////////////////////////////////////////
 // 187. Styles, Attributes and Classes
+/*
 //Styles
 message.style.backgroundColor = '#37383d';
 message.style.width = '120%';
@@ -114,3 +115,35 @@ logo.classList.contains('c'); // not includes
 
 // Don't use
 logo.className = 'jonas';
+*/
+//////////////////////////////////////////////////////////////////
+// 188. Implementing Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  // console.log(e.target.getBoundingClientRect());
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling;
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
