746a5c900314b5320ac46d9f56ac788078c95754
glurmxm <glurmxm@gmail.com>
2023-02-25 18:58:26 -0500

    Section 13: Advanced DOM and Events - 189. Types of Events and Event Handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -118,6 +118,7 @@ logo.className = 'jonas';
 */
 //////////////////////////////////////////////////////////////////
 // 188. Implementing Smooth Scrolling
+/*
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
@@ -147,3 +148,17 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+*/
+//////////////////////////////////////////////////////////////////
+// 189. Types of Events and Event Handlers
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+h1.addEventListener('mouseenter', alertH1);
+
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
