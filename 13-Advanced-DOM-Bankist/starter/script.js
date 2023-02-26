599c1355c9f72d2e3a20eb5a5953b75f19749e14
glurmxm <glurmxm@gmail.com>
2023-02-25 20:57:35 -0500

    Section 13: Advanced DOM and Events - 191. Event Propagation in Practice

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -151,6 +151,7 @@ btnScrollTo.addEventListener('click', function (e) {
 */
 //////////////////////////////////////////////////////////////////
 // 189. Types of Events and Event Handlers
+/*
 const h1 = document.querySelector('h1');
 
 const alertH1 = function (e) {
@@ -162,3 +163,24 @@ h1.addEventListener('mouseenter', alertH1);
 //   alert('onmouseenter: Great! You are reading the heading :D');
 // };
 setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+*/
+//////////////////////////////////////////////////////////////////
+// 190. Event Propagation: Bubbling and Capturing
+
+//////////////////////////////////////////////////////////////////
+// 191. Event Propagation in Practice
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {});
