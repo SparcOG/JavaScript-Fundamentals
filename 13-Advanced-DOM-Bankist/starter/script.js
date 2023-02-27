78df3b586e24c4d03099daf8410317c1f82645ce
glurmxm <glurmxm@gmail.com>
2023-02-27 18:50:22 -0500

    Section 13: Advanced DOM and Events - 193. DOM Traversing

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -61,7 +61,7 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 //////////////////////////////////////////////////////////////////
 // 186. Selecting, Creating, and Deleting Elements
-
+/*
 // Selecting elements
 console.log(document.documentElement);
 console.log(document.head);
@@ -95,7 +95,7 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
-
+*/
 //////////////////////////////////////////////////////////////////
 // 187. Styles, Attributes and Classes
 /*
@@ -199,6 +199,7 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 
 //////////////////////////////////////////////////////////////////
 // 191. Event Propagation in Practice
+/*
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
@@ -222,6 +223,7 @@ document.querySelector('.nav').addEventListener('click', function (e) {
   this.style.backgroundColor = randomColor();
   console.log('NAV', e.target, e.currentTarget);
 });
+*/
 //////////////////////////////////////////////////////////////////
 // 192. Event Delegation: Implementing Page Navigation
 
@@ -237,7 +239,7 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 // });
 
 // 2. Determine what element originated the event
-
+/*
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
   // Matching strategy
@@ -247,4 +249,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
       behavior: 'smooth',
     });
   }
+});
+*/
+//////////////////////////////////////////////////////////////////
+// 193. DOM Traversing
+const h1 = document.querySelector('h1');
+
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
 });
