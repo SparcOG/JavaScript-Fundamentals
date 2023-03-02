db7e11925c701555805d8683dc585286e92aa1d9
glurmxm <glurmxm@gmail.com>
2023-03-02 18:16:54 -0500

    Section 13: Advanced DOM and Events - 194. Building a Tabbed Component

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -305,4 +305,15 @@ tabsContainer.addEventListener('click', function (e) {
 */
 //////////////////////////////////////////////////////////////////
 // 194. Building a Tabbed Component
-nav.addEventListener('mouseover', function (e) {});
+nav.addEventListener('mouseover', function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opcity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+  }
+});
