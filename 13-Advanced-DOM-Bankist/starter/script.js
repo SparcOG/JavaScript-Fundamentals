4e82dbc3952becb966b321fb64254f72b061eea8
glurmxm <glurmxm@gmail.com>
2023-03-05 13:00:20 -0500

    Section 13: Advanced DOM and Events - 197. A Better Way: The Intersection Observer API

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -337,16 +337,21 @@ window.addEventListener('scroll', function () {
 */
 //////////////////////////////////////////////////////////////////
 // 197. A Better Way: The Intersection Observer API
-const obsCallback = function (entries, observer) {
-  entries.forEach(entry => {
-    console.log(entry);
-  });
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entries);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
 };
 
-const obsOptions = {
+const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
-  threshold: [0, 0.2],
-};
+  thershold: 0,
+  rootMargin: `-${navHeight}px`,
+});
 
-const observer = new IntersectionObserver(obsCallback, obsOptions);
-observer.observe(section1);
+headerObserver.observe(header);
