38bdb013268563b3ca85e9de77440094a40f295c
glurmxm <glurmxm@gmail.com>
2023-03-05 15:51:04 -0500

    Section 13: Advanced DOM and Events - 199. Lazy Loading Images

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -359,6 +359,7 @@ headerObserver.observe(header);
 */
 //////////////////////////////////////////////////////////////////
 // 198. Revealing Elements on Scroll
+/*
 const allSections = document.querySelectorAll('.section');
 
 const revealSection = function (entries, observer) {
@@ -379,3 +380,29 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+*/
+//////////////////////////////////////////////////////////////////
+// 199. Lazy Loading Images
+const imgTargets = document.querySelectorAll('img[data-src]');
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  thershold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
