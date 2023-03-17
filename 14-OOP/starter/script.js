aed0c7e033e5eb040f748cecdbf7923ee8667029
glurmxm <glurmxm@gmail.com>
2023-03-16 21:01:01 -0400

    Section 13: Advanced DOM and Events - 214. Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -79,7 +79,7 @@ console.dir(x => x + 1);
 */
 //////////////////////////////////////////////////////////////////
 //  212. Coding Challenge #1
-
+/*
 // 1.
 const car = function (make, speed) {
   this.make = make;
@@ -116,3 +116,65 @@ lada.accelerate();
 lada.accelerate();
 lada.brake();
 lada.brake();
+*/
+//////////////////////////////////////////////////////////////////
+//  213. ES6 Classes
+// class declaration
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Methods will be added to .prototype property
+  cllcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(' ')) this.fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.cllcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// }
+jessica.greet();
+
+//////////////////////////////////////////////////////////////////
+//  214. Setters and Getters
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
