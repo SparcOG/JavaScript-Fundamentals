0d333d9392e64c4e528e50978afb1dd00a32e392
glurmxm <glurmxm@gmail.com>
2023-03-27 18:39:58 -0400

    Section 13: Advanced DOM and Events - 223. Encapsulation: Protected Properties and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -467,31 +467,38 @@ jay.cllcAge();
 */
 //////////////////////////////////////////////////////////////////
 //  222. Another Class Example
+//////////////////////////////////////////////////////////////////
+//  223. Encapsulation: Protected Properties and Methods
+
 class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
-    this.movements = [];
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this._movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -507,6 +514,7 @@ acc1.deposit(250);
 acc1.withdraw(250);
 acc1.requestLoan(1000);
 acc1.approveLoan(1000);
+console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
