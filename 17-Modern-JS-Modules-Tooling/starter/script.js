0184ad881762631d29006c6caad4a5100c9ba76a
SparcOG <96662655+SparcOG@users.noreply.github.com>
2023-07-21 15:54:14 -0400

    Add files via upload

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -9,12 +9,12 @@
 // // ShoppingCart.addToCart('bread', 5);
 // // console.log(ShoppingCart.totalPrice);
 
-// import add, { cart } from './shoppingCart.js';
-// add('pizza', 2);
-// add('bread', 5);
-// add('apples', 4);
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
 
-// console.log(cart);
+console.log(cart);
 
 // // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
 // // const data = await res.json();
@@ -66,11 +66,56 @@ console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
 */
 
-export.addToCart = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+// export.addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart (shipping cost is ${shippingCost})`);
+// }
+
+// const { addToCart } = require('./shoppingCart.js');
+
+// console.log("Учеба работы с терминалом");
+
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
 }
 
-const { addToCart } = require('./shoppingCart.js');
+console.log(
+  'Пришлось прейти на 2 версию из за проблем с зависимостями, команды не запускались и добавить устаревший пакет модуля в windwos 11 чтобы заработало'
+);
 
-console.log("Учеба работы с терминалом");
+class Person {
+  greeting = 'Hey';
+  constructor(name) {
+    this.name = name;
+    console.log(`${this.greeting}, ${this.name}`);
+  }
+}
+
+const jonas = new Person('Jonas');
+
+console.log('Jonas' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+
+// Polyfilling async functions
+import 'regenerator-runtime/runtime';
