cdc36e1388c6b3c9d75db4a89d20a9e71774c53b
SparcOG <96662655+SparcOG@users.noreply.github.com>
2023-07-26 00:02:29 +0000

    Нужно загрузить проект в код спейс, пока это плохо получилось

--- 17-Modern-JS-Modules-Tooling/starter/clean.js
+++ 17-Modern-JS-Modules-Tooling/starter/clean.js
@@ -34,28 +34,31 @@ const newBudget1 = addExpense(budget, spendinglimits, 10, 'Pizza 🍕');
 const newBudget2 = addExpense(newBudget1, spendinglimits, 100, 'Going to movies 🍿', 'Matilda');
 const newBudget3 = addExpense(newBudget2, spendinglimits, 200, 'Stuff', 'Jay');
 
-const checkExpense1 = function (state, limits) {
-  state.map(entry => {
-     entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: 'limit' } : entry;
-  });
-};
 
-const checkExpense = function (state, limits) {
-  state.map(entry => {
-     entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: 'limit' } : entry;
-  });
-};
+// const checkExpense = function (state, limits) {
+//   return state.map(entry => {
+//     return entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: 'limit' } : entry;
+//   });
+// };
+
+const checkExpense = (state, limits) => state.map(entry => entry.value < -getLimit(limits, entry.user) ? { ...entry, flag: 'limit' } : entry);
+
+
 const finalBudget = checkExpense(newBudget3, spendinglimits);
 console.log(finalBudget);
 
 
-const logBigExpenses = function (bigLimit) {
-  let output = '';
-  for (const entry of budget)
-    output += entry.value - bigLimit ? `${entry.description.slice(-2)} /` : ''; // Emojis are 2 chars
-  output = output.slice(0, -2); // Remove last '/ '
-  console.log(output);
+const logBigExpenses = function (state, bigLimit) {
+  // let output = '';
+  // for (const entry of budget)
+  //   output += entry.value - bigLimit ? `${entry.description.slice(-2)} /` : ''; // Emojis are 2 chars
+  // output = output.slice(0, -2); // Remove last '/ '
+  // console.log(output);
+  const bigExpenses = state.filter(entry =>  entry.value  <= -bigLimit).map(entry => entry.description.slice(-2)).join(' / ');
+  console.log(bigExpenses);
 };
 
-logBigExpenses(500);
+logBigExpenses(finalBudget, 500);
 console.log('Hello world!');
+
+console.log('Нужно загрузить проект в код спейс, пока это плохо получилось');
